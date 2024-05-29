52b0a16e57002db6fb5145cd136a9488a6fe4216
ge90114b <ge90114b@outlook.com>
2024-05-29 11:37:28 +0800

    qtfix

--- piying_processpack/qtnode.py
+++ piying_processpack/qtnode.py
@@ -5,12 +5,73 @@ from std_msgs.msg import String
 from std_msgs.msg import Header
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy 
 from cv_bridge import CvBridge
-from piying_processpack.qtfrontend import *
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtWidgets, QtGui
+from sys import exit, argv
 import sys
 
 app = QApplication(sys.argv)
 
+class Ui_MainWindow(object):
+    #UI显示 目前没做窗口位置固定
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(832, 599)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, -10, 781, 581))
+        self.horizontalLayoutWidget = QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setObjectName(u"horizontalLayoutWidget")
+        self.horizontalLayoutWidget.setGeometry(QRect(0, 500, 801, 71))
+        self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")   #按钮1
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.pushButton_2 = QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")   #按钮2
+
+        self.horizontalLayout.addWidget(self.pushButton_2)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 832, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"opencvLabel", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+    # retranslateUi
+
+    #openCV显示主函数
+    def ShowCV(self,cv_image):
+        _image = QtGui.QImage(cv_image[:], cv_image[1], cv_image[0], cv_image[1] * 3, QtGui.QImage.Format_RGB888) #pyqt5转换成自己能放的图片格式
+        jpg_out = QtGui.QPixmap(_image).scaled(self.label.width(), self.label.height()) #设置图片大小
+        self.label.setPixmap(jpg_out) #设置图片显示
+        QApplication.processEvents()
 
 
 class QtFrontendNode(Node):
