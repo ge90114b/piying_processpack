e529f88a50b78f89f6c79821385e999ec166cfdd
ge90114b <ge90114b@outlook.com>
2024-05-30 21:56:53 +0800

    test

--- piying_processpack/corenode.py
+++ piying_processpack/corenode.py
@@ -92,7 +92,7 @@ class CoreNode(Node):
         points=list(points)
         try:
             for i1 in [0,5,6,9,10]:
-                print('first:  ',i1,points[i1])
+                print('first:  ',i1,points[i1],type(points[i1]))
                 time.sleep(1)
                 for i2 in [0,1]:
                     print("second:   ",i2,points[i1][i2])
